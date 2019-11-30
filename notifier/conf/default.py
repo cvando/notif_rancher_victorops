3668463e8200840778dde8c9576dab60de43c730
clement.vandoolaeghe.e <clement.vandoolaeghe.e@thalesdigital.io>
2019-12-01 00:08:36 +0100

    fix typo vars

--- notifier/conf/default.py
+++ notifier/conf/default.py
@@ -2,7 +2,7 @@ import os
 
 victorops_username = os.getenv('VICTOROPS_USERNAME')
 victorops_type = os.getenv('VICTOROPS_TYPE')
-victorops_slug = os.getenv('VICTOROPS_victorops_slug')
+victorops_slug = os.getenv('VICTOROPS_SLUG')
 victorops_apid = os.getenv('VICTOROPS_API_ID')
 victorops_apik = os.getenv('VICTOROPS_API_KEY')
 channels = os.getenv('CHANNELS')
