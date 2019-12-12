9371403f8fbc8655ceabf16d9026ed7f87eba8f9
clement.vandoolaeghe.e <clement.vandoolaeghe.e@thalesdigital.io>
2019-12-12 20:59:06 +0100

    Fix typo http2

--- notifier/main.py
+++ notifier/main.py
@@ -3,7 +3,7 @@ import os
 import conf.default as cfg
 import time
 from env_vars import env_files
-from http_server2 import listen
+from http_server import listen
 
 while "CHANNELS" not in os.environ:
   env_files()
