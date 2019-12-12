ef31c0c742259b92c5c746a881e4dc10c2cd37b1
clement.vandoolaeghe.e <clement.vandoolaeghe.e@thalesdigital.io>
2019-12-12 11:00:39 +0100

    Enable http1.1

--- notifier/http_server.py
+++ notifier/http_server.py
@@ -3,18 +3,24 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from rancher import routing
 
 class S(BaseHTTPRequestHandler):
+    protocol_version = 'HTTP/1.1'
+
     def _set_response(self):
-        msg = "ok"
         self.send_response(200)
         self.send_header("Content-type", "text/html")
+        self.send_header("Content-Length", "2")
         self.end_headers()
 
     def do_GET(self):
         self._set_response()
+        msg = "ok".encode('utf-8')
+        self.wfile.write(msg)
         print("GET request "+str(self.path), flush=True)
 
     def do_POST(self):
+        msg = "ok".encode('utf-8')
         self._set_response()
+        self.wfile.write(msg)
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
         routing(post_data)
