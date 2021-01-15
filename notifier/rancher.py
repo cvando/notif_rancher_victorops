a12c5ac1927d89c35784d2bcb07bac2307f56e6f
clement vandoolaeghe <clement.vandoolaeghe@ercom.fr>
2021-01-15 18:12:32 +0100

    Alerts without template

--- notifier/rancher.py
+++ notifier/rancher.py
@@ -12,38 +12,38 @@ def routing(data):
     if 'status' in data:
       status = data['status']
       for i in status:
-      name = data['alerts'][i]['labels']['alert_name']
-      cluster = data['alerts'][i]['labels']['prometheus_from']
-      date = data['alerts'][i]['startsAt']
-      target = data['alerts'][i]['labels']['description']
-      namespace = data['alerts'][i]['labels']['namespace']
-
-
-      summary = cluster+" "+namespace+" "+name+" "+target
-      content = "Status: "+status+"\nAlert: "+name+"\nFirst seen: "+date+"\nCluster: "+cluster+"\nNamespace: "+namespace+"\nTarget: "+name+" "+target+"\nEvent: "+msg
-      issue = [date, name, target]
-      str = ' '.join(issue)
-      varhash = hash(str)
+        name = data['alerts'][i]['labels']['alert_name']
+        cluster = data['alerts'][i]['labels']['prometheus_from']
+        date = data['alerts'][i]['startsAt']
+        target = data['alerts'][i]['labels']['description']
+        namespace = data['alerts'][i]['labels']['namespace']
   
-      if status == "firing":
-        if str not in issues:
-          issues.append(str)
-          if "victorops" in cfg.channels:
-            tickets[varhash] = firetovictorops(name, content, summary)
-          if "citadel" in cfg.channels:
-            firetocitadel(content)
-        
-      if status == "resolved":
-        for i in issues:
-          if str == i:
-            issues.remove(str)
+  
+        summary = cluster+" "+namespace+" "+name+" "+target
+        content = "Status: "+status+"\nAlert: "+name+"\nFirst seen: "+date+"\nCluster: "+cluster+"\nNamespace: "+namespace+"\nTarget: "+name+" "+target+"\nEvent: "+msg
+        issue = [date, name, target]
+        str = ' '.join(issue)
+        varhash = hash(str)
+    
+        if status == "firing":
+          if str not in issues:
+            issues.append(str)
             if "victorops" in cfg.channels:
-              if varhash in tickets:
-                numissue = tickets[varhash]
-                resolvetovictorops(numissue)
-                del tickets[varhash]
+              tickets[varhash] = firetovictorops(name, content, summary)
             if "citadel" in cfg.channels:
               firetocitadel(content)
+          
+        if status == "resolved":
+          for i in issues:
+            if str == i:
+              issues.remove(str)
+              if "victorops" in cfg.channels:
+                if varhash in tickets:
+                  numissue = tickets[varhash]
+                  resolvetovictorops(numissue)
+                  del tickets[varhash]
+              if "citadel" in cfg.channels:
+                firetocitadel(content)
 
   except KeyError:
     print("Malformed json:", flush=True)
